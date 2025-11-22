15cc55ba4254f37b0ac7e7116980504dd010d720
Lando <olandomcdonald07@gmail.com>
2025-11-21 23:58:17 -0600

    Change App.test.tsx

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,10 +2,10 @@ import React from "react";
 import { render, screen } from "@testing-library/react";
 import App from "./App";
 
-test("renders basic info section with name", () => {
+test("renders basic info section with college name", () => {
     render(<App />);
     const linkElement = screen.getByText((content) =>
-        content.includes("Olando"),
+        content.includes("Red River College"),
     );
     expect(linkElement).toBeInTheDocument();
 });
